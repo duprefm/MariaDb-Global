c38e5d9889ddd08d290b661ca81193b527de5f2b
Linux User <duprefm@gmail.com>
2017-11-12 14:40:06 +0000

    Rename file

--- nginx-galera/run.sh
+++ nginx-galera/run.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-if [ ! -z "$node1" ]
+echo "$node1 : $node2 : $node3"
+
+if [ "$node1" == "node1" ]
 then
-	"node1"="GALERACLUSTER_node1"
-	sed -i 's/node1/'"$node1"'/g' /etc/nginx/nginx.conf 
+	node1="node1"
 else
-	"node1"="$node1"
-	sed -i 's/node1/'"$node1"'/g' /etc/nginx/nginx.conf 
+	node1="$node1"
 fi
 
-sed -i 's/node1/'"$node1"'/g' /etc/nginx/nginx.conf 
-sed -i 's/node2/'"$node2"'/g' /etc/nginx/nginx.conf 
-sed -i 's/node3/'"$node3"'/g' /etc/nginx/nginx.conf 
+if [ "$node2" == "node2" ]
+then
+        node2="node2"
+else
+        node2="$node2"
+fi
+
+if [ "$node3" == "node3" ]
+then
+        node3="node3"
+else
+        node3="$node3"
+fi
+
+cat /etc/nginx/nginx.conf | sed -e "s/node1/"${node1}"/g" -e "s/node2/"${node2}"/g" -e "s/node3/"${node3}"/g" > /etc/nginx/nginx.conf.new
+mv /etc/nginx/nginx.conf.new /etc/nginx/nginx.conf
+#sed -i 's/node1/'"$node1"'/g' /etc/nginx/nginx.conf 
+#sed -i 's/node2/'"$node2"'/g' /etc/nginx/nginx.conf 
+#sed -i 's/node3/'"$node3"'/g' /etc/nginx/nginx.conf 
