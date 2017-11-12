5b7ddadafb35b8856443f8adf1ce8b9fe3049228
Linux User <duprefm@gmail.com>
2017-11-12 11:41:18 +0000

    Ajout sed dans run.sh nginx-galera

--- nginx-galera/run.sh
+++ nginx-galera/run.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ ! -z "$node1" ]
+then
+	"node1"="GALERACLUSTER_node1"
+	sed -i 's/node1/'"$node1"'/g' /etc/nginx/nginx.conf 
+else
+	"node1"="$node1"
+	sed -i 's/node1/'"$node1"'/g' /etc/nginx/nginx.conf 
+fi
+
 sed -i 's/node1/'"$node1"'/g' /etc/nginx/nginx.conf 
 sed -i 's/node2/'"$node2"'/g' /etc/nginx/nginx.conf 
 sed -i 's/node3/'"$node3"'/g' /etc/nginx/nginx.conf 
