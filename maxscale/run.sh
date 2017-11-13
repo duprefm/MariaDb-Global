cf6b2247856ecd8438cffcdef3f5c7c9895ed9c7
fabricedupre <32126510+duprefm@users.noreply.github.com>
2017-11-13 07:52:59 +0100

    Update run.sh

--- maxscale/run.sh
+++ maxscale/run.sh
@@ -6,11 +6,13 @@ echo $address3  address3 >> /etc/hosts
 
 sed -i 's/user=$user/user='"$user"'/g' /usr/local/skysql/maxscale/etc/MaxScale.cnf
 sed -i 's/passwd=$passwd/passwd='"$passwd"'/g' /usr/local/skysql/maxscale/etc/MaxScale.cnf
-- address1=GALERACLUSTER_node1
+sed -i 's/address1=GALERACLUSTER_node1
       - address2=GALERACLUSTER_node2
       - address3=GALERACLUSTER_node3
       - user=cluster
       - passwd=clusterpass
+      - MYSQL_DATABASE=test
+      - MYSQL_USER=test_user
 
 sleep 300
 #/usr/bin/maxscale -f /usr/local/skysql/maxscale/etc/MaxScale.cnf
