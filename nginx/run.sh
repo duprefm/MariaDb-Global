5d48505db1b95c3b2a2b2f9c6b0c648917311f7c
fabricedupre <32126510+duprefm@users.noreply.github.com>
2017-11-17 21:10:02 +0100

    Update run.sh

--- nginx/run.sh
+++ nginx/run.sh
@@ -5,4 +5,4 @@ echo $app_name
 sed -i 's/template/'"$app_name"'/g' /etc/nginx/nginx.conf
 
 #nginx -s reload -c /etc/nginx/nginx.conf
-nginx -c /etc/nginx/nginx.conf
+#nginx -c /etc/nginx/nginx.conf
