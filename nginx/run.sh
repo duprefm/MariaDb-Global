4f006360cbf36ba7495d2d8cc482e60f6b624f51
root <duprefm@gmail.com>
2017-11-17 22:23:08 +0000

    bla

--- nginx/run.sh
+++ nginx/run.sh
@@ -4,5 +4,7 @@ echo $app_name
 
 sed -i 's/template/'"$app_name"'/g' /etc/nginx/nginx.conf
 
-#nginx -s reload -c /etc/nginx/nginx.conf
-#nginx -c /etc/nginx/nginx.conf
+sleep 30
+nginx -t
+nginx -s stop
+nginx -c /etc/nginx/nginx.conf
